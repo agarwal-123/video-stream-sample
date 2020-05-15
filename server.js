4b5bd037f8749acd4688f25ad820949898a86131
agarwal-123 <43489651+agarwal-123@users.noreply.github.com>
2020-05-15 23:53:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/video', function(req, res) {
     fs.createReadStream(path).pipe(res)
   }
 })
-
-app.listen(3000, function () {
+const port=process.env.PORT || 3000
+app.listen(port, function () {
   console.log('Listening on port 3000!')
 })
